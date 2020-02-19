71491e761f8c35a2ea8057f17b5b0a09bf2d091d
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2020-02-19 09:21:31 -0600

    Datos disponibles en csv, rds y feather

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# coordenadas_metro_cdmx
-Coordenadas del Metrod de la Ciudad de México
+# Coordenadas de las estaciones del Metro de la Ciudad de México (CDMX)
+
+Conjunto de datos con las coordenadas de las estaciones del Metro de la Ciudad de México. Incluye datos de las estaciones de las líneas 1 a 12.
+
+La información de las coordenadas ha sido extraída de Wikipedia en Español, tomando como referencia el listado de estaciones siguiente:
+
+* https://es.wikipedia.org/wiki/Anexo:Estaciones_de_metro_de_la_Ciudad_de_México
+
+Los datos se encuentran disponibles en tres formatos de archivo diferentes.
+
+* estaciones_coords.csv - Valores separados por comas.
+* estaciones_coords.rds - Archivo compatible con R.
+* estaciones_coorda.feather - Archivo feather, compatible con Python y R, requiere el paquete Feather para su lectura (https://github.com/wesm/feather)
+
+Además de dos archivos usados en la extracción de datos:
+ 
+ * extraccion.R - Código usado para hacer scrapping de datos con R.
+ * estaciones_html.rds - HTML usado como base para el scapping de datos, compatible con R.
--- extraccion.R
+++ extraccion.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(rvest)
 library(janitor)
+library(feather)
 
 estaciones <- read_html(x = "https://es.wikipedia.org/wiki/Anexo:Estaciones_de_metro_de_la_Ciudad_de_M%C3%A9xico")
 
@@ -11,42 +12,47 @@ estaciones_nombres <-
   clean_names() %>% 
   as_tibble()
 
-safe_html <- safely(read_html, otherwise = "404")
-estaciones_html_test <- map(estaciones_urls, safe_html)
-map(estaciones_html_test, "error")
+# # Proceso para identificar urls no validos
+# estaciones_urls <- 
+# estaciones_nombres$estacion %>% 
+# str_remove("/.*") %>% 
+# str_replace_all(" ", "_") %>% 
+# paste0("https://es.wikipedia.org/wiki/", ., "_(estación)")
+# 
+# safe_html <- safely(read_html, otherwise = "404")
+# estaciones_html_test <- map(estaciones_urls, safe_html)
+# map(estaciones_html_test, "error")
 
 estaciones_urls <- 
   estaciones_nombres$estacion %>% 
   str_remove("/.*") %>% 
   str_replace_all(" ", "_") %>% 
   paste0("https://es.wikipedia.org/wiki/", ., "_(estación)") %>% 
-  str_replace("(Universidad|Tasqueña|Sevilla|San_(Antonio|Joaquín)|Portales|Candelaria)_\\(estación\\)", 
-              "\\1_(estación_del_Metro_de_Ciudad_de_México)")
-
-safe_html <- safely(read_html, otherwise = "404")
-estaciones_html_test <- map(estaciones_urls, safe_html)
-map(estaciones_html_test, "error")
+  str_replace("(Tasqueña|Sevilla|San_(Antonio|Joaquín)|Portales|Candelaria)_\\(estación\\)", 
+              "\\1_(estación_del_Metro_de_Ciudad_de_México)") %>% 
+  str_replace("(Universidad)_\\(estación\\)", 
+              "\\1_(estación_del_Metro_de_la_Ciudad_de_México)")
 
 estaciones_html <- map(estaciones_urls, read_html)
-
-write_rds(estaciones_html, "estaciones_html.rds")
 
 estaciones_coords <- 
   map_chr(estaciones_html, function(estacion) {
     estacion %>% 
       html_nodes(".nourlexpansion") %>% 
-      html_text() %>% 
-      str_replace_all("\\D", ".") %>% 
-      str_replace_all("\\.{2,}", "-") %>% 
-      `[`(1)
+      html_text()  %>% 
+      str_extract("(?<=/).*") %>% 
+      `[`(1) %>% 
+      str_trim()
   }) %>% 
   tibble(coords = .) %>% 
-  separate(coords, into = c("a", "b", "lng", "lat"), sep = "-") %>%
+  separate(coords, into = c("lng", "lat"), sep = ",") %>% 
   mutate_all(as.numeric) %>% 
-  mutate(lat = lat * -1,
-         estacion = estaciones_nombres$estacion) %>% 
-  select(estacion, lng, lat)
+  mutate(estacion = estaciones_nombres$estacion) %>% 
+  select(estacion, lng, lat) %>%
+  data.frame()
 
-
-
-
+write_feather(estaciones_coords, "estaciones_coords.feather")
+write.csv(estaciones_coords, "estaciones_coords.csv", 
+          row.names = FALSE, fileEncoding = "utf-8")
+write_rds(estaciones_html, "estaciones_html.rds")
+write_rds(estaciones_coords, "estaciones_coords.rds")
